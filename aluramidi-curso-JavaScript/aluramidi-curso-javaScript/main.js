3ce577c6c56913224d0a395bac8b1b4cbc4e2275
ophilipelima <philipehenri.lima@gmail.com>
2025-02-08 18:03:00 -0300

    finalizado

--- aluramidi-curso-JavaScript/aluramidi-curso-javaScript/main.js
+++ aluramidi-curso-JavaScript/aluramidi-curso-javaScript/main.js
@@ -1,5 +1,13 @@
 function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+       elemento.play();
+    }
+
+    else{
+        console.log('Elemento não encontrado');
+    }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -14,5 +22,17 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
+
+    tecla.onkeydown = function(evento){
+        console.log(evento.code);
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
 
 }
