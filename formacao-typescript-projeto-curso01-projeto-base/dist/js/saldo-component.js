09d6c8a29e546931959e47be090847728def7a40
ophilipelima <philipehenri.lima@gmail.com>
2025-05-20 22:56:38 -0300

    addFuncoesDeDataEsaldo

--- formacao-typescript-projeto-curso01-projeto-base/dist/js/saldo-component.js
+++ formacao-typescript-projeto-curso01-projeto-base/dist/js/saldo-component.js
@@ -3,14 +3,9 @@ let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" }); //Exibir saldo em formato de moeda
+    elementoSaldo.textContent = formatarMoeda(saldo); //Chamando função para exibir saldo em formato de moeda
 }
 if (elementoSaldo != null) {
     const dataAcesso = new Date(); // Exibir data atual
-    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
-        weekday: "long",
-        day: "2-digit",
-        month: "2-digit",
-        year: "numeric"
-    });
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
 }
--- formacao-typescript-projeto-curso01-projeto-base/src/saldo-component.ts
+++ formacao-typescript-projeto-curso01-projeto-base/src/saldo-component.ts
@@ -5,15 +5,10 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLEleme
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
 if(elementoSaldo != null){
-    elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency"}); //Exibir saldo em formato de moeda
+    elementoSaldo.textContent = formatarMoeda(saldo) //Chamando função para exibir saldo em formato de moeda
 }
 
 if(elementoSaldo != null){
     const dataAcesso: Date = new Date(); // Exibir data atual
-    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
-        weekday: "long",
-        day: "2-digit", 
-        month: "2-digit",
-        year: "numeric"
-    });
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
 }
