82b09fb3acd7bedc68fe2c259743987ca5932cd2
ophilipelima <philipehenri.lima@gmail.com>
2025-07-16 15:13:51 -0300

    criandoComponenteSeparador

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalioComponent } from './componentes/cabecalio/cabecalio.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalioComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet, 
+    ContainerComponent, 
+    CabecalioComponent,
+    SeparadorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
