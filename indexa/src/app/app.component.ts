416c0f795adb6306b1a37a8a125591858de74ce3
ophilipelima <philipehenri.lima@gmail.com>
2025-07-16 23:52:11 -0300

    percorrendoAlfabeto

--- indexa/src/app/app.component.ts
+++ indexa/src/app/app.component.ts
@@ -18,5 +18,5 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
 }
