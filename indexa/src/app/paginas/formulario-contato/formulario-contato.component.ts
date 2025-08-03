3c2f5338ba8cd5d559c5980d16ca63a3b1a8504d
ophilipelima <philipehenri.lima@gmail.com>
2025-08-02 22:20:58 -0300

    sincronizandoFormulario

--- indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ indexa/src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,17 +2,34 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
   imports: [CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+  contatoForm!: FormGroup
 
+  constructor() {
+        this.contatoForm = new FormGroup({
+          nome: new FormControl('Philipe'),
+          telefone: new FormControl('99 99999-9999'),
+          email: new FormControl('philipe@gmail.com'),
+          aniversario: new FormControl(''),
+          redes: new FormControl(''),
+          observacoes: new FormControl('')
+    })
+  }
+
+  salvarContato(){
+    console.log(this.contatoForm.value);
+  }
 }
